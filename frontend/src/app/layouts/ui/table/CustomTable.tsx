e1227ea4bc4774950ecf0409a967f4c956b4b40b
airacalins <airacalins@gmail.com>
2022-01-23 00:04:10 +0800

    style slots table

---
+++ frontend/src/app/layouts/ui/table/CustomTable.tsx
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { TableCellProps } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import CustomTablePagination from '../tablePagination/CustomTablePagination';
+import { TableHead } from '@mui/material';
+import { CSSProperties } from '@mui/styled-engine';
+
+function createData(name: string, calories: number, fat: number) {
+  return { name, calories, fat };
+}
+
+const rows = [
+  createData('Cupcake', 305, 3.7),
+  createData('Donut', 452, 25.0),
+  createData('Eclair', 262, 16.0),
+  createData('Frozen yoghurt', 159, 6.0),
+  createData('Gingerbread', 356, 16.0),
+  createData('Honeycomb', 408, 3.2),
+  createData('Ice cream sandwich', 237, 9.0),
+  createData('Jelly Bean', 375, 0.0),
+  createData('KitKat', 518, 26.0),
+  createData('Lollipop', 392, 0.2),
+  createData('Marshmallow', 318, 0),
+  createData('Nougat', 360, 19.0),
+  createData('Oreo', 437, 18.0),
+].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+
+
+
+interface CustomTableProps {
+  columns: Array<{title: string; style?: any}>;
+  rows: JSX.Element[];
+}
+
+const CustomTable = ({ columns, rows }: CustomTableProps) => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }}>
+        <TableHead>
+          <TableRow>
+            {columns.map((column, index) => (
+              <TableCell key={index} style={{...column.style, fontSize: 14}} >
+                {column.title}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => row)}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={columns.length}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={CustomTablePagination}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default CustomTable;
--- frontend/src/features/slot/SlotTable.tsx
+++ frontend/src/features/slot/SlotTable.tsx
@@ -1,52 +1,82 @@
-import { Paper } from "@mui/material";
-import { DataGrid, GridActionsCellItem, GridValueGetterParams } from "@mui/x-data-grid";
-import { Label, Table } from "semantic-ui-react";
-import SearchBar from "../../app/layouts/components/SearchBar";
-import TableBody from "../../app/layouts/components/table/TableBody";
-import TableComponent from "../../app/layouts/components/table/TableComponent";
-import TableHeader from "../../app/layouts/components/table/TableHeader";
-import { currencyFormatter } from "../../app/layouts/formatter/common";
-import { ISlot } from "../../app/models/slot";
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import history from '../../app/utils/history';
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import { ISlot } from '../../app/models/slot';
+import CustomTablePagination from '../../app/layouts/ui/tablePagination/CustomTablePagination';
+import CustomTable from '../../app/layouts/ui/table/CustomTable';
+import { GridActionsCellItem, GridValueGetterParams } from '@mui/x-data-grid';
+import { Label } from 'semantic-ui-react';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import history from '../../app/utils/history';
+import './slot.scss';
 
 interface Props {
   slots: ISlot[]
 }
 
+function createData(name: string, calories: number, fat: number) {
+  return { name, calories, fat };
+}
+
+const rows = [
+  createData('Cupcake', 305, 3.7),
+  createData('Donut', 452, 25.0),
+  createData('Eclair', 262, 16.0),
+  createData('Frozen yoghurt', 159, 6.0),
+  createData('Gingerbread', 356, 16.0),
+  createData('Honeycomb', 408, 3.2),
+  createData('Ice cream sandwich', 237, 9.0),
+  createData('Jelly Bean', 375, 0.0),
+  createData('KitKat', 518, 26.0),
+  createData('Lollipop', 392, 0.2),
+  createData('Marshmallow', 318, 0),
+  createData('Nougat', 360, 19.0),
+  createData('Oreo', 437, 18.0),
+].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+
 const SlotTable = ({ slots }: Props) => {
-  const columns = [
-    { field: 'slotNumber', headerName: 'Slot Number', minWidth: 130 },
-    { field: 'size', headerName: 'Size', minWidth: 130 },
-    { field: 'price', headerName: 'Rental Fee', minWidth: 130 },
-    {
-      field: 'status',
-      headerName: 'Status',
-      minWidth: 130,
-      renderCell: (params: GridValueGetterParams) => <Label content={params.row.tenantContract ? "Rented" : "Available"} color={params.row.tenantContract ? "blue" : "green"}></Label>
-    },
-    {
-      field: 'id',
-      type: 'actions',
-      width: 100,
-      getActions: (params: GridValueGetterParams) => [
-        <GridActionsCellItem icon={<VisibilityIcon />} label="Edit" onClick={() => history.push(`/slots/${params.row.id}/details`)}/>
-      ],
-    },
+    const columns = [
+    { title: 'Slot Number'},
+    { title: 'Size'},
+    { title: 'Rental Fee' },
+    { title: 'Status' },
+    { title: '' },
   ]
+
   return (
-    <Paper>
-      <div style={{ height: 700, width: '100%' }}>
-        <DataGrid
-          rows={slots}
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[10]}
-        />
-      </div>
-    </Paper>
+    <CustomTable
+      columns={columns}
+      rows={slots.map(slot => <TableRow key={slot.id}>
+        <TableCell component="td" scope="row">
+          {slot.slotNumber}
+        </TableCell>
+        <TableCell style={{ width: 50 }} align="right">
+          {slot.size}
+        </TableCell>
+        <TableCell style={{ width: 100 }} align="right">
+          {slot.price}
+        </TableCell>
+        <TableCell style={{ width: 160 }} align="left">
+          <Label content={slot.tenantContract ? "Rented" : "Available"} color={slot.tenantContract ? "blue" : "green"}></Label>
+        </TableCell>
+        <TableCell style={{ width: 50 }} align="right">
+          <VisibilityIcon onClick={() => history.push(`/slots/${slot.id}/details`)} />
+        </TableCell>
+      </TableRow>)}
+    />
   );
 }
 
