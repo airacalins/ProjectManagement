695be4ca7fe52bfca8979b517d2e9efcda072bc8
Aira Calingasan <airacalins@gmail.com>
2022-01-26 01:43:46 +0800

    mode of payment

--- frontend/src/features/modeOfPayment/ModeOfPayment.tsx
+++ frontend/src/features/modeOfPayment/ModeOfPayment.tsx
@@ -1,36 +1,85 @@
-import { useEffect } from "react";
-import ContainerPage from "../../app/layouts/components/container/ContainerPage";
-import Tab from "../../app/layouts/components/tabs/Tab";
-import TabButton from "../../app/layouts/components/tabs/TabButton";
-import TabItem from "../../app/layouts/components/tabs/TabItem";
-import ModeOfPaymentTable from "./ModeOfPaymentTable";
-import LoadingComponent from "../../app/layouts/components/loading/LoadingComponent";
+import { useEffect, useMemo, useState } from "react";
 import { useAppDispatch, useAppSelecter } from "../../app/store/configureStore";
 import { fetchModeOfPaymentsAsync } from "./modeOfPaymentSlice";
+import history from '../../app/utils/history';
+
+import { Icon, Label } from 'semantic-ui-react';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import NavigateNextOutlinedIcon from '@mui/icons-material/NavigateNextOutlined';
+
+import LoadingComponent from "../../app/layouts/components/loading/LoadingComponent";
+import MainPage from "../../app/layouts/components/pages/MainPage";
+import CustomTable from "../../app/layouts/components/table/CustomTable";
 
 const ModeOfPayment = () => {
+    const [searchKey, setSearchKey] = useState('');
+    const { modeOfPayments, isFetching: isFetchingModeOfPayments } = useAppSelecter(state => state.modeOfPayment);
 
-    const {modeOfPayments, isFetching } = useAppSelecter(state => state.modeOfPayment);
     const dispatch = useAppDispatch();
-  
+
+    const data = useMemo(() => {
+        if (!!searchKey) {
+            return modeOfPayments.filter(i => i.bankName.toLowerCase().includes(searchKey.toLowerCase()));
+        }
+        return modeOfPayments;
+    }, [modeOfPayments, searchKey])
+
     useEffect(() => {
-      dispatch(fetchModeOfPaymentsAsync());
+        dispatch(fetchModeOfPaymentsAsync());
     }, [])
-    
-    if (isFetching) return <LoadingComponent content="Loading Mode of Payments..." />
+
+    const columns = [
+        { title: 'Bank Name' },
+        { title: 'Account Name' },
+        { title: 'Account Number' },
+        { title: 'Show' },
+        { title: '' },
+    ]
+
+    if (isFetchingModeOfPayments) return <LoadingComponent content="Loading Mode of Payments..." />
 
     return (
-        <ContainerPage children={
-            <>
-                <Tab>
-                    <TabItem name="All" navigateTo="/mode-of-payments" />
-                    <TabItem name="Disabled" navigateTo="./mode-of-payments/disabled" />
-                    <TabButton name="Add Mode of Payment" navigateTo="/mode-of-payments/create" />
-                </Tab>
+        <MainPage
+            title="Mode of Payments"
+            content={
+                <CustomTable
+                    columns={columns}
+                    rows=
+                    {
+                        modeOfPayments.map(mop =>
+                            <TableRow key={mop.id}>
 
-                <ModeOfPaymentTable modeOfPayments={modeOfPayments} />
-            </>
-        } />
+                                <TableCell align="center">
+                                    {mop.bankName}
+                                </TableCell>
+
+                                <TableCell align="center">
+                                    {mop.accountName}
+                                </TableCell>
+
+                                <TableCell align="center">
+                                    {mop.accountNumber}
+                                </TableCell>
+
+                                <TableCell align="center">
+                                    <Icon name="toggle on" color="blue" size="large"></Icon>
+                                </TableCell>
+
+                                <TableCell align="right">
+                                    <NavigateNextOutlinedIcon onClick={() => history.push(`/mode-of-payments/${mop.id}/details`)} />
+                                </TableCell>
+
+                            </TableRow>
+                        )
+                    }
+                    searchValue={searchKey}
+                    onSearch={(value: string) => setSearchKey(value)}
+                    buttonTitle="Create Slot"
+                    navigateTo="/slots/create"
+                />
+            }
+        />
     );
 }
 
