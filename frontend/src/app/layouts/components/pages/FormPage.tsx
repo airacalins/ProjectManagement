68545bdc4e67a6ea102fdb893399c0a9a8e348f0
Aira Calingasan <airacalins@gmail.com>
2022-01-31 22:10:40 +0800

    added invoice items to invoice

--- frontend/src/app/layouts/components/pages/FormPage.tsx
+++ frontend/src/app/layouts/components/pages/FormPage.tsx
@@ -6,23 +6,28 @@ import './page.scss'
 
 interface Props {
     backNavigationLink?: string
-    title: string
+    title?: string
     form: React.ReactNode,
 }
 
 const FormPage: React.FC<Props> = ({ backNavigationLink, title, form }) => {
     return (
-        <div>
+        <div className="mb-5">
             {
                 backNavigationLink &&
-                <Button className="page d-flex align-items-center px-4 mb-1" as={Link} to={backNavigationLink}>
+                <Button className="page d-flex align-items-center px-4" as={Link} to={backNavigationLink}>
                     <MenuOpenOutlinedIcon />
                 </Button>
             }
 
-            <h3 className="page__title w-100 d-flex align-items-center px-4">{title}</h3>
+            {
+                title &&
+                <h3 className="page__title w-100 d-flex align-items-center px-4">
+                    {title}
+                </h3>
+            }
 
-            <div className="page__container px-5 py-4 mx-5 my-4">
+            <div className="page__container px-5 py-4 mx-5">
                 {form}
             </div>
         </div >
--- frontend/src/app/models/invoice.ts
+++ frontend/src/app/models/invoice.ts
@@ -7,20 +7,31 @@ export interface IInvoice {
   businessName: string,
   slotNumber: string,
   amount: number,
-  payments: [
-    {
-      id: string,
-      dateCreated: Date,
-      status: PaymentStatus,
-      bankName: string,
-      accountName: string,
-      accountNumber: string,
-      amount: number
-    }
-  ],
+  payments: IPayment[],
+  invoiceItems: IInvoiceItem[],
   dateCreated: Date,
-  dueDate: Date
+  dueDate: Date,
+  invoiceNumber: string
 }
+
+export interface IPayment {
+  id: string,
+  status: number,
+  bankName: string,
+  accountName: string,
+  accountNumber: string,
+  dateCreated: Date,
+  amount: number,
+  referenceNumber: string
+}
+
+export interface IInvoiceItem {
+  id: string,
+  description: string,
+  amount: number
+}
+
+
 
 export interface IUpdatePaymentStatusModel {
   id: string,
