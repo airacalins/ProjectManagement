b1ee06b69d6cb8325ad04d4dcb7f4ac071e3f29c
airacalins <airacalins@gmail.com>
2022-02-03 13:12:15 +0800

    update invoice service

--- API/Services/InvoiceService.cs
+++ API/Services/InvoiceService.cs
@@ -63,40 +63,63 @@ namespace API.Services
         _context.InvoiceItems.Add(invoiceItem);
         await _context.SaveChangesAsync();
 
+        //Create invoice items for last invoice late payment.
+        //invoices are generated montly, so no need to check if last invoice late days is greater than 60 days
         if (lastInvoice != null)
         {
+          //logic for paid invoice and payment date is greater than due date
           if (lastInvoice.LastPaymentDate != null)
           {
             if (lastInvoice.LastPaymentDate > lastInvoice.DueDate)
             {
               var lateDays = lastInvoice.LastPaymentDate.Value.Subtract(lastInvoice.DueDate).Days;
-              var latePenalty = lateDays < 60 ? 0.03 : 0.08;
+              var latePenalty = 0.05;
               var latePenaltyPayment = new InvoiceItem
               {
                 InvoiceId = invoice.Id,
-                Description = lateDays < 60 ? $"Late Payment Fee (5%)" : $"Late Payment Fee (8%)",
+                Description = $"Late Payment Fee (5%)",
                 Amount = contract.Price + (contract.Price * latePenalty)
               };
               _context.InvoiceItems.Add(latePenaltyPayment);
               await _context.SaveChangesAsync();
             }
           }
+          //logic for unpaid invoice
           if (lastInvoice.LastPaymentDate == null && lastInvoice.DueDate < DateTimeOffset.UtcNow)
           {
             var lateDays = DateTimeOffset.UtcNow.Subtract(lastInvoice.DueDate).Days;
-            var latePenalty = lateDays < 60 ? 0.03 : 0.08;
+            var latePenalty = 0.05;
             var latePenaltyPayment = new InvoiceItem
             {
               InvoiceId = invoice.Id,
-              Description = lateDays < 60 ? $"Late Payment Fee (5%)" : $"Late Payment Fee (8%)",
+              Description = $"Late Payment Fee for {lastInvoice.InvoiceNumber}(5%)",
               Amount = contract.Price + (contract.Price * latePenalty)
             };
             _context.InvoiceItems.Add(latePenaltyPayment);
             await _context.SaveChangesAsync();
-
           }
         }
 
+        var previousInvoices = await _context.Invoices.Include(i => i.InvoiceItems).Where(i => i.InvoiceStatus != InvoiceStatus.Paid)
+        .OrderByDescending(i => i.DateCreated)
+        .Where(i => i.TenantContractId == contract.Id).ToListAsync();
+
+        //Create invoice items for 2 months late payment
+        foreach(var prevInvoice in previousInvoices)
+        {
+            var lateDays = DateTimeOffset.UtcNow.Subtract(prevInvoice.DueDate).Days;
+          if (lateDays >= 60) {
+            
+            var latePenaltyPayment = new InvoiceItem
+            {
+              InvoiceId = invoice.Id,
+              Description = lateDays < 60 ? $"Late Payment Fee for {prevInvoice.InvoiceNumber}(5%)" : $"Late Payment Fee {prevInvoice.InvoiceNumber}(8%)",
+              Amount = contract.Price + (contract.Price * 0.08)
+            };
+            _context.InvoiceItems.Add(latePenaltyPayment);
+            await _context.SaveChangesAsync();
+          }
+        }
 
         var nextBillDate = contract.NextPaymentDate.AddMonths(1);
         if (nextBillDate > contract.EndDate)
