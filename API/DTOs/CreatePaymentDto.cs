7efa6cab51cf63d065d23c42822aec65d3432b83
airacalins <airacalins@gmail.com>
2022-02-15 03:03:53 +0800

    add form name

--- API/DTOs/CreatePaymentDto.cs
+++ API/DTOs/CreatePaymentDto.cs
@@ -8,10 +8,14 @@ namespace API.DTOs
 {
     public class CreatePaymentDto
     {
+        [FromForm(Name = "invoiceId")]
         public Guid InvoiceId { get; set; }
+        [FromForm(Name = "modeOfPaymentId")]
         public Guid ModeOfPaymentId { get; set; }
+        [FromForm(Name = "amount")]
         public double Amount { get; set; }
         public string? FileString { get; set; }
+        [FromForm(Name = "file")]
         public FormFile File { get; set; }
     }
 }
